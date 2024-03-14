83c2bedfa7668b919ffefc4076c3812b2d7b540d
skylerisadev <154493205+skylerisadev@users.noreply.github.com>
2024-03-14 03:00:04 -0700

    Add files via upload

---
+++ settings/main.js
@@ -0,0 +1,15 @@
+console.warn(
+    "%cHello!!", "color: red; font-weight: 600; background: yellow; padding: 0 5px; border-radius: 5px",
+    "Shack!"
+  );
+  
+  const local_title = localStorage.getItem("title");
+  const local_icon = localStorage.getItem("icon");
+  if (window.localStorage.hasOwnProperty("title")) {
+    document.title = local_title;
+    console.log("Title set to: " + local_title);
+  }
+  if (window.localStorage.hasOwnProperty("icon")) {
+    document.querySelector("link[rel=icon]").href = local_icon;
+    console.log("Icon set to: " + local_icon);
+  }
